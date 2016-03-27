5904e8227e2ff4433f450f2539b79bde167f11d7
James Chambers <james@jameschambers.com>
2016-03-27 17:03:36 -0500

    initial commit

---
+++ src/IrrelevantCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Asp.Net.Monsters;
+
+namespace IrrelevantCode
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var mid = "250ZXN0LmF6dXJld2Vic2l0ZXMubmV0Lw";
+            var term = "==";
+            var whysosnoopy = "aHR0cDovL3RoaXNpc25vdGFjb";
+            var notusedanywhere = Unused.Value();
+            Console.WriteLine($"{whysosnoopy}{mid}{term}");
+        }
+    }
+
+
+}
---
+++ src/IrrelevantCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IrrelevantCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IrrelevantCode")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c7c244e3-b36b-41d8-b519-2eb0ab1a9b02")]
